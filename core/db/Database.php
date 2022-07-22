319aa5c6bff1d4bb34aead40993ea95ae27e2ff3
Sos89 <sosmanucharyan89@gmail.com>
2022-07-22 16:40:46 +0400

    ---

--- core/db/Database.php
+++ core/db/Database.php
@@ -2,7 +2,6 @@
 
 namespace app\core\db;
 
-use app\core\Application;
 
 class Database
 {
@@ -18,71 +17,6 @@ class Database
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
 
-    public function applyMigrations()
-    {
-        $this->createMigrationsTable();
-        $appliedMigrations = $this->getAppliedMigrations();
-
-        $newMigrations = [];
-
-        $files = scandir(Application::$ROOT_DIR.'/migrations');
-        $toApplyMigrations = array_diff($files, $appliedMigrations);
-
-       foreach ($toApplyMigrations as $migration)
-       {
-           if ($migration === '.' || $migration === '..')
-           {
-               continue;
-           }
-
-           require_once Application::$ROOT_DIR.'/migrations/'.$migration;
-           $className = pathinfo($migration, PATHINFO_FILENAME);
-            $instance = new $className();
-            $this->log("Applying migration $migration");
-            $instance->up();
-            $this->log("Applied migration $migration");
-            $newMigrations[] = $migration;
-       }
-
-       if (!empty($newMigrations)){
-           $this->saveMigrations($newMigrations);
-       }else{
-           $this->log("All migrations are applied");
-       }
-    }
-
-    public function createMigrationsTable(){
-        $this->pdo->exec("CREATE TABLE IF NOT EXISTS 
-                migrations (
-                id INT AUTO_INCREMENT PRIMARY KEY, 
-                migration VARCHAR(255),
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                ) ENGINE=INNODB;");
-    }
-
-    public function getAppliedMigrations()
-    {
-        $statement = $this->pdo->prepare("SELECT migration From migrations");
-        $statement->execute();
-
-        return $statement->fetchAll(\PDO::FETCH_COLUMN);
-    }
-
-//    public function select(){
-//        $select = $this->pdo->prepare("SELECT * FROM products ORDER BY create_data DESC ");
-//        $select->execute();
-//        $select->fetchAll(\PDO::FETCH_ASSOC);
-//    }
-
-    public function saveMigrations(array $migrations)
-    {
-        $str = implode(",", array_map(fn($m) => "('$m')", $migrations));
-
-        $statement = $this->pdo->prepare("INSERT INTO migrations (migration) VALUES 
-            $str                          
-        ");
-        $statement->execute();
-    }
 
     public function prepare($sql)
     {
